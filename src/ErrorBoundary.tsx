637eb11139dc67709912de41f8a3140d836f1310
Yury Andrabaila <yandrabaila@gmail.com>
2024-07-10 22:11:36 +0200

    fix: linter and husky

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+  errorLogged: boolean;
+};
+
+type ErrorBoundaryProps = {
+  children: React.ReactNode;
+};
+
+export default class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false, errorLogged: false };
+  }
+
+  componentDidCatch(error: Error) {
+    const { errorLogged } = this.state;
+    if (!errorLogged) {
+      console.error(error);
+      this.setState({ hasError: true, errorLogged: true });
+    }
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      return (
+        <div className="error">
+          <h1>ERROR!!!</h1>
+          <button
+            type="button"
+            className="button-reload"
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            Reload
+          </button>
+        </div>
+      );
+    }
+    return children;
+  }
+}

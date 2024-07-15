10ef373cfdd42089ac37f77f68f5635c314e82ae
Yury Andrabaila <yandrabaila@gmail.com>
2024-07-15 20:16:03 +0200

    refactor: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './app/App.tsx';
+import App from './app/App';
 import { Provider } from 'react-redux';
-import { store } from './store.ts';
+import { store } from './store';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  );
+} else {
+  console.error('Root element not found');
+}

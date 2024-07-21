246bb4d92b7557466a0539fc5f462bfb9329be20
Yury Andrabaila <yandrabaila@gmail.com>
2024-07-21 21:48:17 +0200

    fix: 404 page

--- src/pages/404/Page404.tsx
+++ src/pages/404/Page404.tsx
@@ -1,10 +1,18 @@
 export default function Page404() {
+  const handleClick = () => {
+    window.location.href = '/';
+  };
+
   return (
     <main>
+      <h1>404</h1>
       <p>Sorry, there is no such page.</p>
       <p>Прабачце, такой старонкі не існуе.</p>
       <p>Извините, такой страницы не существует.</p>
       <p>Przepraszam, nie ma takiej strony.</p>
+      <button type="button" onClick={handleClick}>
+        Go to main page
+      </button>
     </main>
   );
 }

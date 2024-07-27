cf4a13706be9e36ee39e0acfc4e34b9974094815
Yury Andrabaila <yandrabaila@gmail.com>
2024-07-27 22:48:22 +0200

    feat: add jest

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.app.json',
+      },
+    ],
+  },
+  setupFilesAfterEnv: ['./src/setupTests.ts'],
+};
+
+export default config;

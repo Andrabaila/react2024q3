3ffcb93454445341b08e532ade3d4c586fcf5c4a
Yury Andrabaila <yandrabaila@gmail.com>
2024-07-28 12:45:48 +0200

    feat: add test 404

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
 
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
